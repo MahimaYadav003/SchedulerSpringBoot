f8b55f10220e0e63358996fce466ec35f98437cb
MahimaYadav003 <mahimayadav_18041@aitpune.edu.in>
2021-11-25 15:42:04 +0530

    changes postgreSQL configuration

--- src/main/java/com/springrestscheduler/scheduler/Dao/SchedulerDao.java
+++ src/main/java/com/springrestscheduler/scheduler/Dao/SchedulerDao.java
@@ -1,6 +1,8 @@
 package com.springrestscheduler.scheduler.Dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.springrestscheduler.scheduler.entities.Students;
 public interface SchedulerDao extends JpaRepository<Students, Long>{
 
